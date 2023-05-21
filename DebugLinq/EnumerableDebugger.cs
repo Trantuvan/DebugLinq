14e6f15346036fd1ccc176ba18a0271816230d66
trantuvan <trantuvan.kan@gmail.com>
2023-05-21 16:46:09 +0700

    Add project files.

---
+++ DebugLinq/EnumerableDebugger.cs
@@ -0,0 +1,36 @@
+﻿namespace DebugLinq;
+
+public static class EnumerableDebugger
+{
+    public static ConsoleColor DefaultColor = ConsoleColor.Yellow;
+    public static bool ShowWhiteSpace { get; set; }
+
+    public static IEnumerable<T> Dump<T>(this IEnumerable<T> input)
+    {
+        return Dump(input,
+                    item => item is not null ? item.ToString() : "(null)",
+                    DefaultColor);
+    }
+
+    public static IEnumerable<T> Dump<T>(this IEnumerable<T> input,
+                                          ConsoleColor consoleColor)
+    {
+        return Dump(input,
+                    item => item is not null ? item.ToString() : "(null)",
+                    consoleColor);
+    }
+
+    public static IEnumerable<T> Dump<T>(this IEnumerable<T> input,
+                                          Func<T, string> ToString,
+                                          ConsoleColor consoleColor)
+    {
+        foreach (var item in input)
+        {
+            Console.ForegroundColor = consoleColor;
+            Console.WriteLine(
+                ShowWhiteSpace ? '[' + ToString(item) + ']' : ToString(item));
+            Console.ResetColor();
+            yield return item;
+        }
+    }
+}
---
+++ DebugLinq/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace DebugLinq;
+
+public class Program
+{
+    static readonly string[] Words = new string[] { "   KOOKABURRA", "Frogmouth", "kingfisher   ", "loon", "merganser" };
+    static void Main(string[] args)
+    {
+        var newWords = ProcessWordList3(Words);
+
+        foreach (var word in newWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+
+    private static IEnumerable<string> ProcessWordList1(string[] words)
+    {
+        return words
+            .Select(w => w.Trim())
+            .Select(w => w.ToLower())
+            .Where(w => w.StartsWith("k"))
+            .OrderBy(w => w);
+    }
+
+    private static IEnumerable<string> ProcessWordList2(string[] words)
+    {
+        EnumerableDebugger.ShowWhiteSpace = true;
+
+        return words
+            .Dump()
+            .Select(w => w.Trim())
+            .Dump()
+            .Select(w => w.ToLower())
+            .Dump()
+            .Where(w => w.StartsWith("k"))
+            .Dump()
+            .OrderBy(w => w)
+            .Dump();
+    }
+
+    private static IEnumerable<string> ProcessWordList3(string[] words)
+    {
+        EnumerableDebugger.ShowWhiteSpace = true;
+
+        return words
+            .Dump(w => "ORIGINAL: " + w, ConsoleColor.Yellow)
+            .Select(w => w.Trim())
+            .Dump(w => "TRIMMED: " + w, ConsoleColor.Yellow)
+            .Select(w => w.ToLower())
+            .Dump(w => "LOWERCASE: " + w, ConsoleColor.Green)
+            .Where(w => w.StartsWith("k"))
+            .Dump(w => "FILTERD: " + w, ConsoleColor.Red)
+            .OrderBy(w => w)
+            .Dump(w => "SORTED: " + w, ConsoleColor.Blue);
+    }
+}
